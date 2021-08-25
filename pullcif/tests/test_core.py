5f6622061455e8f2cea4b28d93f04fbc5e224e65
st3107 <st3107@columbia.edu>
2021-08-25 14:50:43 -0400

    TST: add loadStructure tests back

--- pullcif/tests/test_core.py
+++ pullcif/tests/test_core.py
@@ -1,6 +1,7 @@
 import pytest
 
 import pullcif.core as core
+from diffpy.structure import loadStructure, Structure
 
 
 @pytest.skip
@@ -9,3 +10,7 @@ def test_main(tmp_path):
     # there are files output
     fs = list(tmp_path.glob("*"))
     assert len(fs) > 0
+    # all files can be loaded by diffpy.structure
+    for f in tmp_path.glob("*"):
+        stru = loadStructure(str(f), fmt="cif")
+        assert isinstance(stru, Structure)
