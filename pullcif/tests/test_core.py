6135a474ba83aec3bbf560fa95f94e7bdcd436b2
st3107 <st3107@columbia.edu>
2021-08-25 14:05:34 -0400

    TST: not to use diffpy loadStructure to test it

--- pullcif/tests/test_core.py
+++ pullcif/tests/test_core.py
@@ -9,7 +9,3 @@ def test_main(tmp_path):
     # there are files output
     fs = list(tmp_path.glob("*"))
     assert len(fs) > 0
-    # all files can be loaded by diffpy.structure
-    for f in tmp_path.glob("*"):
-        stru = loadStructure(str(f), fmt="cif")
-        assert isinstance(stru, Structure)
