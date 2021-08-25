52dd3ae30f92ff84fa3caac0901a780dc330fc3b
st3107 <st3107@users.noreply.github.com>
2021-08-25 20:23:23 +0000

    deploy: 93ee060d08fd7ec581a565dddf718a94900a281e

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+gfb66f1e',
+    VERSION: '0.post1+g93ee060',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
