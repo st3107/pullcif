32c8da71fa4eadd067b137316b48c1267b33613f
st3107 <st3107@columbia.edu>
2021-08-26 20:32:44 -0400

    BLD: no diffpy

--- pullcif/tests/test_core.py
+++ pullcif/tests/test_core.py
@@ -1,7 +1,6 @@
 import pytest
 
 import pullcif.core as core
-from diffpy.structure import loadStructure, Structure
 
 
 @pytest.mark.skip
@@ -10,7 +9,3 @@ def test_main(tmp_path):
     # there are files output
     fs = list(tmp_path.glob("*"))
     assert len(fs) > 0
-    # all files can be loaded by diffpy.structure
-    for f in tmp_path.glob("*"):
-        stru = loadStructure(str(f), fmt="cif")
-        assert isinstance(stru, Structure)
