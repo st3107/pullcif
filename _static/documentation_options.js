8fe2fc73e8c20b2a57c621864f70dfe0e484e4e8
Fengkai-Wei <Fengkai-Wei@users.noreply.github.com>
2021-08-28 18:27:16 +0000

    deploy: 5d2d33b91d0c820f40bb1a0c1412a48ab264450f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g32c8da7',
+    VERSION: '0.post1+g5d2d33b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
